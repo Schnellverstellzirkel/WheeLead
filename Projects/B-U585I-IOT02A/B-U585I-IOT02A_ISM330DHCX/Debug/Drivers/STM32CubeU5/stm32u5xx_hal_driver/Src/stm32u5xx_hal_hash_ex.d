7ff2064ae1408e9cc72682d91e36b4c9106c6b0c
Schnellverstellzirkel <ettalebe@gmail.com>
2024-01-15 11:11:45 +0100

    first commit

---
+++ Projects/B-U585I-IOT02A/B-U585I-IOT02A_ISM330DHCX/Debug/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Src/stm32u5xx_hal_hash_ex.d
@@ -0,0 +1,58 @@
+Drivers/STM32CubeU5/stm32u5xx_hal_driver/Src/stm32u5xx_hal_hash_ex.o: \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Src/stm32u5xx_hal_hash_ex.c \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal.h \
+ ../Core/Inc/stm32u5xx_hal_conf.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_rcc.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_def.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/stm32u5xx.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/stm32u585xx.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/core_cm33.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_version.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/mpu_armv8.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/system_stm32u5xx.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_rcc_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_gpio.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_gpio_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_dma.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_dma_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_cortex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_flash.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_flash_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_i2c.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_i2c_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_pwr.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_pwr_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_uart.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_uart_ex.h \
+ /home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_exti.h
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal.h:
+../Core/Inc/stm32u5xx_hal_conf.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_rcc.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_def.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/stm32u5xx.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/stm32u585xx.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/core_cm33.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_version.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/CMSIS/Core/Include/mpu_armv8.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/cmsis_device_u5/Include/system_stm32u5xx.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_rcc_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_gpio.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_gpio_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_dma.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_dma_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_cortex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_flash.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_flash_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_i2c.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_i2c_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_pwr.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_pwr_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_uart.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_uart_ex.h:
+/home/ubuntu/Documents/ISMIN_3A/INOVGAMES/code/Drivers/STM32CubeU5/stm32u5xx_hal_driver/Inc/stm32u5xx_hal_exti.h:
